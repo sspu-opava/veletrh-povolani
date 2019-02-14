4ff4e98d33e5ae230a64f1ef8460beddc975b6a8
sspu-opava <itworkshop@sspu-opava.cz>
2019-02-14 23:26:02 +0100

    Databaze

--- js/firmy.js
+++ js/firmy.js
@@ -1,34 +1,314 @@
 var firmy = [
-    {
-      "firma": "Brano Group a.s.",
-      "url": "http://www.brano.eu"
-    },
-    {
-      "firma": "OSTROJ a.s.",
-      "url": "http://www.ostroj.cz"
-    },
-    {
-      "firma": "Ferram a.s.",
-      "url": "http://ferram.cz"
-    },
-    {
-      "firma": "Prestar s.r.o.",
-      "url": "http://prestar.cz"
-    },
-    {
-      "firma": "Strojírna Vehovský s.r.o.",
-      "url": "http://www.vehovsky.cz"
-    },
-    {
-      "firma": "ARMATURY Group a.s.",
-      "url": "http://www.armaturygroup.cz"
-    },
-    {
-      "firma": "Model Obaly, a. s.",
-      "url": "http://www.modelgroup.com/cs/about/locations/model-obaly-skl%C3%A1da%C4%8Dky"
-    },
-    {
-      "firma": "Hon a.s.",
-      "url": "http://www.hon.cz"
-    }
-  ]
+  {
+    "url": "http://www.adiv.cz",
+    "firma": "ADIV, spol. s r.o."
+  },
+  {
+    "url": "http://www.interpon.cz/interpon_series/interpon-100/",
+    "firma": "Akzo Nobel Coatings CZ, a.s."
+  },
+  {
+    "url": "http://www.jirchaart.cz/",
+    "firma": "Anna Kolichová – JIRCHA ART"
+  },
+  {
+    "url": "http://www.oblouky.cz/",
+    "firma": "Antonín Kostrhoun"
+  },
+  {
+    "url": "http://www.armaturygroup.cz/",
+    "firma": "ARMATURY Group a.s."
+  },
+  {
+    "url": "https://www.firmy.cz/detail/254473-aron-bensch-opava-predmesti.html",
+    "firma": "Aron Bensch"
+  },
+  {
+    "url": "https://www.prepis-textu.cz/",
+    "firma": "Bc. Pavla Žídková"
+  },
+  {
+    "url": "http://www.benkoklima.cz/",
+    "firma": "BENKO KLIMA s.r.o."
+  },
+  {
+    "url": "https://www.bohemiaflex-cs.cz/",
+    "firma": "BOHEMIAFLEX CS, s.r.o."
+  },
+  {
+    "url": "http://www.brano.eu/",
+    "firma": "BRANO a.s."
+  },
+  {
+    "url": "https://www.csob.cz/",
+    "firma": "ČSOB, a.s., pobočka Opava"
+  },
+  {
+    "url": "http://www.pavelek.cz/",
+    "firma": "ELEKTRO – FA. PAVELEK, s.r.o."
+  },
+  {
+    "url": "https://elok.cz/",
+    "firma": "Elok-Opava spol. s r.o."
+  },
+  {
+    "url": "https://www.femont.cz/",
+    "firma": "FEMONT OPAVA s.r.o."
+  },
+  {
+    "url": "http://www.ferram.cz/",
+    "firma": "FERRAM, a.s."
+  },
+  {
+    "url": "http://www.fros.cz/",
+    "firma": "FROS ZPS s.r.o."
+  },
+  {
+    "url": "http://www.hagemann.cz/",
+    "firma": "HAGEMANN a.s."
+  },
+  {
+    "url": "http://www.hansen-electric.cz/",
+    "firma": "Hansen Electric, spol. s r.o."
+  },
+  {
+    "url": "http://www.hokej.cz/klub/4",
+    "firma": "Hokejový klub Opava s.r.o."
+  },
+  {
+    "url": "http://www.hon.cz/",
+    "firma": "HON a.s."
+  },
+  {
+    "url": "http://www.av-market.cz/",
+    "firma": "Ing. Antonín Volný"
+  },
+  {
+    "url": "https://www.dane-a-ucetnictvi.eu/",
+    "firma": "Ing. Dagmar Grafová"
+  },
+  {
+    "url": "http://www.kamaxopava.cz/www/main.php",
+    "firma": "Ing. Jiří Balhar"
+  },
+  {
+    "url": "ttps://www.firmy.cz/detail/407949-ing-jiri-kastovsky-opava-male-hostice.html",
+    "firma": "Ing. Jiří Kaštovský"
+  },
+  {
+    "url": "http://www.bozpoaisofuchs.cz/",
+    "firma": "Ing. Vladimír Fuchs"
+  },
+  {
+    "url": "http://www.cmiel.cz/",
+    "firma": "Ing.Vladislav Čmiel"
+  },
+  {
+    "url": "https://iqkuchyne.cz/#uvod",
+    "firma": "IQ kuchyně"
+  },
+  {
+    "url": "https://www.isotra.cz/",
+    "firma": "ISOTRA a.s."
+  },
+  {
+    "url": "http://www.pneu-stop.cz/kontakt",
+    "firma": "Jan Schindler"
+  },
+  {
+    "url": "http://www.odry.cz/ing-jindrich-sedlar-financni-poradce/os-103326",
+    "firma": "JINDŘICH SEDLÁŘ"
+  },
+  {
+    "url": "http://www.jany.cz/",
+    "firma": "Jiří Jany"
+  },
+  {
+    "url": "http://www.komas.cz/cs/",
+    "firma": "KOMAS, spol. s r.o."
+  },
+  {
+    "url": "http://www.ksrint.com/",
+    "firma": "KSR Industrial s.r.o."
+  },
+  {
+    "url": "http://www.vahy.net/",
+    "firma": "Ladislav Kadlec"
+  },
+  {
+    "url": "http://www.pavelek.cz/",
+    "firma": "Lukáš Pavelek"
+  },
+  {
+    "url": "http://www.prejda.cz/",
+    "firma": "Lumír Prejda"
+  },
+  {
+    "url": "https://www.knappe.cz/",
+    "firma": "Martin Knappe"
+  },
+  {
+    "url": "http://www.mlynherber.cz/",
+    "firma": "MLÝN HERBER spol. s.r.o."
+  },
+  {
+    "url": "http://www.modelgroup.com/cs",
+    "firma": "Model Obaly a.s."
+  },
+  {
+    "url": "https://www.moraviastamping.com/",
+    "firma": "MORAVIA Stamping a.s."
+  },
+  {
+    "url": "http://www.msa.cz/",
+    "firma": "MSA a.s.&nbsp"
+  },
+  {
+    "url": "http://www.pepasport.cz/",
+    "firma": "NEXT Fitness s.r.o."
+  },
+  {
+    "url": "https://www.rybka-cz.cz/",
+    "firma": "Norbert Rybka"
+  },
+  {
+    "url": "http://www.nord-service.cz/",
+    "firma": "NORD SERVICE, spol. s r.o."
+  },
+  {
+    "url": "https://www.opall-agri.cz/",
+    "firma": "OPaLL-AGRI, s.r.o."
+  },
+  {
+    "url": "http://www.opatisk.cz/",
+    "firma": "OPATISK s.r.o."
+  },
+  {
+    "url": "http://www.opavskalesni.cz/",
+    "firma": "Opavská lesní a.s."
+  },
+  {
+    "url": "http://www.openagency.cz/",
+    "firma": "OPEN AGENCY s.r.o."
+  },
+  {
+    "url": "https://www.optys.cz/",
+    "firma": "OPTYS, spol. s r.o."
+  },
+  {
+    "url": "http://www.vsenaprani.cz/",
+    "firma": "Pavel Kašing"
+  },
+  {
+    "url": "https://www.pekarnakral.cz/",
+    "firma": "PEKÁRNA KRÁL s.r.o."
+  },
+  {
+    "url": "http://www.pfplasty.cz/cs/",
+    "firma": "PF PLASTY s.r.o."
+  },
+  {
+    "url": "https://www.pohl.cz/",
+    "firma": "POHL cz, a.s./odštěpný závod"
+  },
+  {
+    "url": "http://www.pouba.cz/",
+    "firma": "POUBA a.s."
+  },
+  {
+    "url": "http://prestar.cz/",
+    "firma": "Prestar, s.r.o."
+  },
+  {
+    "url": "http://www.primalingua.cz/",
+    "firma": "PRIMA LINGUA s.r.o."
+  },
+  {
+    "url": "http://www.rkl.cz/cs/",
+    "firma": "RKL Opava spol. s r.o."
+  },
+  {
+    "url": "http://www.tomosopava.cz/",
+    "firma": "Rudolf Tomáš"
+  },
+  {
+    "url": "http://sblogistik.sweb.cz/",
+    "firma": "SB LOGISTIK TRANSPORT s.r.o"
+  },
+  {
+    "url": "http://www.secalo.cz/",
+    "firma": "SECALO a.s."
+  },
+  {
+    "url": "https://www.slezska-tvorba.cz/",
+    "firma": "Slezská tvorba, výrobní družstvo"
+  },
+  {
+    "url": "https://www.slu.cz/",
+    "firma": "Slezská univerzita v Opavě"
+  },
+  {
+    "url": "http://www.slezskestavby.cz/",
+    "firma": "SLEZSKÉ STAVBY OPAVA s.r.o."
+  },
+  {
+    "url": "http://www.soukroma-oa-opava.cz/",
+    "firma": "Soukromá obchodní akademie Opava s.r.o."
+  },
+  {
+    "url": "https://www.podnikatelskaskola.cz/",
+    "firma": "Soukromá SŠ podnikatelská, s.r.o., Opava"
+  },
+  {
+    "url": "http://www.soustop.cz/",
+    "firma": "SOU stavební, Opava, p.o."
+  },
+  {
+    "url": "https://www.sshsopava.cz/skola/",
+    "firma": "SŠ hotelnictví a služeb a VOŠ, Opava, p.o."
+  },
+  {
+    "url": "https://www.hssilherovice.cz/",
+    "firma": "SŠ hotelnictví, gastronomie a služeb SČMSD Šilheřovice, s.r.o."
+  },
+  {
+    "url": "https://www.sst.opava.cz/",
+    "firma": "SŠ technická, Opava, Kolofíkovo nábřeží 51, p.o."
+  },
+  {
+    "url": "http://www.vehovsky.cz/",
+    "firma": "STROJÍRNA Vehovský s.r.o."
+  },
+  {
+    "url": "http://www.stenceltransport.cz/",
+    "firma": "ŠTENCEL TRANSPORT s.r.o."
+  },
+  {
+    "url": "http://www.tevapharm.cz/",
+    "firma": "Teva Czech Industries s.r.o."
+  },
+  {
+    "url": "https://www.grafico.cz/",
+    "firma": "TISKÁRNA GRAFICO s.r.o."
+  },
+  {
+    "url": "https://www.tqm.cz/",
+    "firma": "TQM&nbsp"
+  },
+  {
+    "url": "http://www.transa.cz/",
+    "firma": "TRANSA spol. s r.o."
+  },
+  {
+    "url": "http://unicont.cz/",
+    "firma": "UNICONT OPAVA s.r.o."
+  },
+  {
+    "url": "http://www.vzsrychtar.cz/",
+    "firma": "VZS RYCHTÁŘ s.r.o."
+  },
+  {
+    "url": "http://www.winro.cz/",
+    "firma": "Winkler Robert"
+  }
+]
--- js/skoly.js
+++ js/skoly.js
@@ -1,110 +1,178 @@
 var skoly = [
-    {
-      "skola": "Střední škola hotelnictví a služeb a Vyšší odborná škola, Opava",
-      "url": "https://www.sshsopava.cz"
-    },
-    {
-      "skola": "AHOL - střední odborná škola, s.r.o.",
-      "url": "https://www.ahol.cz"
-    },
-    {
-      "skola": "SOŠ dopravy a cestovního ruchu, Krnov, p. o.",
-      "url": "http://www.sos-dcr.cz"
-    },
-    {
-      "skola": "Střední pedagogická škola a Střední zdravotnická škola svaté Anežky České, Odry",
-      "url": "https://www.cssodry.cz"
-    },
-    {
-      "skola": "Střední odborná umělecká škola varhanářská o.p.s, Krnov",
-      "url": "http://www.varhanysous.cz"
-    },
-    {
-      "skola": "Střední škola technická a dopravní, Ostrava-Vítkovice",
-      "url": "https://www.sstd.cz"
-    },
-    {
-      "skola": "Soukromá obchodní akademie Opava s.r.o.",
-      "url": "http://www.soukroma-oa-opava.cz"
-    },
-    {
-      "skola": "Střední škola průmyslová a umělecká, Opava",
-      "url": "https://www.sspu-opava.cz"
-    },
-    {
-      "skola": "Střední průmyslová škola a Obchodní akademie, Bruntál",
-      "url": "http://spsoa.cz"
-    },
-    {
-      "skola": "Sřední škola hotelnictví, gastronomie a služeb SČMSD Šilheřovice, s.r.o.",
-      "url": "https://www.hssilherovice.cz"
-    },
-    {
-      "skola": "Střední škola polygrafická, Olomouc",
-      "url": "http://www.polygraficka.cz"
-    },
-    {
-      "skola": "Církevní konzervatoř Německého řádu, Opava",
-      "url": "http://www.konzervator.cz"
-    },
-    {
-      "skola": "Střední odborná škola, Bruntál",
-      "url": "http://www.sosbruntal.cz"
-    },
-    {
-      "skola": "Střední průmyslová škola stavební, Opava",
-      "url": "http://www.spsopava.cz"
-    },
-    {
-      "skola": "Střední škola, Odry",
-      "url": "http://ssodry.cz"
-    },
-    {
-      "skola": "Odborné učiliště a Praktická škola, Hlučín",
-      "url": "https://www.ouhlucin.cz"
-    },
-    {
-      "skola": "Střední škola informatiky, elektrotechniky a řemesel Rožnov pod Radhoštěm",
-      "url": "https://www.roznovskastredni.cz"
-    },
-    {
-      "skola": "Střední zdravotnická škola a Vyšší odborná škola zdravotnická, Ostrava",
-      "url": "https://www.zdrav-ova.cz"
-    },
-    {
-      "skola": "Střední škola automobilní, Krnov",
-      "url": "https://www.ssa-krnov.cz"
-    },
-    {
-      "skola": "Základní škola a gymnázium Vítkov",
-      "url": "http://zsgvitkov.cz"
-    },
-    {
-      "skola": "Mendelovo gymnázium, Opava",
-      "url": "https://www.mgo.opava.cz"
-    },
-    {
-      "skola": "AVE ART Ostrava, soukromá Střední umělecká škola a Základní umělecká škola, s.r.o.",
-      "url": "http://aveart.cz"
-    },
-    {
-      "skola": "Tauferova SOŠ veterinární Kroměříž",
-      "url": "http://vetkm.cz"
-    },
-    {
-      "skola": "Střední škola průmyslová, Krnov",
-      "url": "https://www.sspkrnov.cz"
-    },
-    {
-      "skola": "Střední odborná škola lesnická a strojírenská, Šternberk",
-      "url": "http://www.sou-stbk.cz"
-    },
-    {
-      "skola": "Střední vinařská škola Valtice",
-      "url": "https://www.svisv.cz"
-    },
-    {
-      "skola": "Střední škola technická, Opava",
-      "url": "https://www.sst.opava.cz"
-    }
-  ]
+  {
+    "skola": "AHOL - střední odborná škola, s.r.o.",
+    "url": "https://www.ahol.cz"
+  },
+  {
+    "skola": "AVE ART Ostrava, soukromá Střední umělecká škola a Základní umělecká škola, s.r.o.",
+    "url": "http://aveart.cz"
+  },
+  {
+    "skola": "Bezpečnostně právní akademie Ostrava, s. r. o., střední škola",
+    "url": "http://www.bpa-ostrava.cz"
+  },
+  {
+    "skola": "Církevní konzervatoř Německého řádu",
+    "url": "http://www.konzervator.cz"
+  },
+  {
+    "skola": "Gymnázium Josefa Kainara, Hlučín",
+    "url": "http://ghlucin.cz"
+  },
+  {
+    "skola": "Masarykova střední škola zemědělská a Vyšší odborná škola, Opava, příspěvková organizace",
+    "url": "http://www.zemedelka.opava.cz"
+  },
+  {
+    "skola": "Mendelovo gymnázium, Opava",
+    "url": "https://www.mgo.opava.cz"
+  },
+  {
+    "skola": "Obchodní akademie a Střední odborná škola logistická, Opava, příspěvková organizace",
+    "url": "https://www.oa-opava.cz"
+  },
+  {
+    "skola": "Odborné učiliště a Praktická škola, Hlučín",
+    "url": "https://www.ouhlucin.cz"
+  },
+  {
+    "skola": "RB SOU autoopravárenské s.r.o.",
+    "url": "http://www.souauto.cz"
+  },
+  {
+    "skola": "Slezské gymnázium, Opava, příspěvková organizace",
+    "url": "http://www.slezgymopava.cz"
+  },
+  {
+    "skola": "SOŠ dopravy a cestovního ruchu, Krnov, p. o.",
+    "url": "http://www.sos-dcr.cz"
+  },
+  {
+    "skola": "SOŠ ochrany a osob majetku s.r.o.",
+    "url": "http://www.sosoom-ostrava.cz"
+  },
+  {
+    "skola": "Soukromá obchodní akademie Opava s.r.o.,",
+    "url": "http://www.soukroma-oa-opava.cz"
+  },
+  {
+    "skola": "Soukromá střední podnikatelská škola, s.r.o.,Opava",
+    "url": "https://www.podnikatelskaskola.cz"
+  },
+  {
+    "skola": "Střední odborná škola a Základní škola, Město Albrechtice, příspěvková organizace",
+    "url": "https://www.souzma.cz"
+  },
+  {
+    "skola": "Střední odborná škola lesnická a strojírenská",
+    "url": "http://www.sou-stbk.cz"
+  },
+  {
+    "skola": "Střední odborná škola, Bruntál, příspěvková organizace",
+    "url": "http://www.sosbruntal.cz"
+  },
+  {
+    "skola": "Střední odborná umělecká škola varhanářská o.p.s",
+    "url": "http://www.varhanysous.cz"
+  },
+  {
+    "skola": "Střední odborné učiliště stavební, Opava, příspěvková organizace",
+    "url": "https://soustop.cz"
+  },
+  {
+    "skola": "Střední pedagogická škola a Střední zdravotnická škola svaté Anežky České",
+    "url": "https://www.cssodry.cz"
+  },
+  {
+    "skola": "Střední pedagogická škola a Střední zdravotnická škola, Krnov",
+    "url": "https://www.spgs-szs.cz/"
+  },
+  {
+    "skola": "Střední průmyslová škola a Obchodní akademie, Bruntál, příspěvková organizace",
+    "url": "http://spsoa.cz"
+  },
+  {
+    "skola": "Střední průmyslová škola chemická akademika Heyrovského",
+    "url": "https://www.spsch.eu"
+  },
+  {
+    "skola": "Střední průmyslová škola Ostrava-Vítkovice, příspěvková organizace",
+    "url": "https://www.spszengrova.cz"
+  },
+  {
+    "skola": "Střední průmyslová škola stavební Opava",
+    "url": "http://www.spsopava.cz"
+  },
+  {
+    "skola": "Střední škola automobilní, Krnov, příspěvková organizace",
+    "url": "https://www.ssa-krnov.cz"
+  },
+  {
+    "skola": "Střední škola elektrotechnická",
+    "url": "http://www.sse-najizdarne.cz"
+  },
+  {
+    "skola": "Střední škola hotelnictví a služeb a Vyšší odborná škola, Opava, příspěvková organizace",
+    "url": "https://www.sshsopava.cz"
+  },
+  {
+    "skola": "Střední škola hotelnictví, gastronomie a služeb SČMSD Šilheřovice, s.r.o.",
+    "url": "https://www.hssilherovice.cz"
+  },
+  {
+    "skola": "Střední škola informatiky, elektrotechniky a řemesel Rožnov pod Radhoštěm",
+    "url": "https://www.roznovskastredni.cz"
+  },
+  {
+    "skola": "Střední škola polygrafická, Olomouc",
+    "url": "http://www.polygraficka.cz"
+  },
+  {
+    "skola": "Střední škola průmyslová a umělecká, Opava",
+    "url": "https://www.sspu-opava.cz"
+  },
+  {
+    "skola": "Střední škola průmyslová, Krnov, příspěvková organizace",
+    "url": "https://www.sspkrnov.cz"
+  },
+  {
+    "skola": "Střední škola technická a dopravní, Ostrava-Vítkovice",
+    "url": "https://www.sstd.cz"
+  },
+  {
+    "skola": "Střední škola technická, Opava, Kolofíkovo nábřeží 51, příspěvková organizace",
+    "url": "https://www.sst.opava.cz"
+  },
+  {
+    "skola": "Střední škola teleinformatiky, Ostrava, příspěvková organizace",
+    "url": "https://teleinformatika.eu"
+  },
+  {
+    "skola": "Střední škola, Odry, příspěvková organizace Moravskoslezského kraje",
+    "url": "http://ssodry.cz"
+  },
+  {
+    "skola": "Střední vinařská škola Valtice",
+    "url": "https://www.svisv.cz"
+  },
+  {
+    "skola": "Střední zdravotnická škola a Vyšší odborná škola zdravotnická, Ostrava, příspěvková organizace",
+    "url": "https://www.zdrav-ova.cz"
+  },
+  {
+    "skola": "Střední zdravotnická škola, Opava, příspěvková organizace",
+    "url": "http://www.szsopava.cz"
+  },
+  {
+    "skola": "Tauferova SOŠ veterinární Kroměříž",
+    "url": "http://vetkm.cz"
+  },
+  {
+    "skola": "Vyšší odborná škola potravinářská a Střední průmyslová škola mlékárenská Kroměříž",
+    "url": "http://www.vospaspsm.cz"
+  },
+  {
+    "skola": "Základní škola a gymnázium Vítkov, příspěvková organizace",
+    "url": "http://zsgvitkov.cz"
+  }
+]
